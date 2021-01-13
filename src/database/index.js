00e3503e77e3738e22aaa74388a0a7c62874a194
hebertcisco <mrrabbitomg@gmail.com>
2021-01-13 01:14:57 -0300

    created: database configuration

---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+const mongodb = require("../config/mongodb");
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.mongoConnection = mongodb;
+  }
+}
+module.exports = new Database();
