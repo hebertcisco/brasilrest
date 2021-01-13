e41cce4f305e843e48efda95e8b5649afba01dee
hebertcisco <mrrabbitomg@gmail.com>
2021-01-13 01:29:55 -0300

    add: database status

--- src/controllers/StatusController.js
+++ src/controllers/StatusController.js
@@ -1,3 +1,5 @@
+const mongodb = require("../config/mongodb");
+
 class StatusController {
   async show(req, res) {
     res.status(200);
@@ -5,6 +7,11 @@ class StatusController {
       status: "online",
       date: new Date(),
       environment: process.env.NODE_ENV,
+      database: {
+        status: await mongodb
+          .then(() => "Connected! <3")
+          .catch((err) => `Disconnected )-; ${err}`),
+      },
       aws: {
         region: process.env.AWS_REGION || "local",
         function_version: process.env.AWS_LAMBDA_FUNCTION_VERSION || "local",
